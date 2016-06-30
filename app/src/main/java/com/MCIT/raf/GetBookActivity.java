2f7fdb76af3c210a3098d482442fb8cf1a21dbca
agamal94 <a.gamal94@hotmail.com>
2016-06-30 19:23:42 +0200

    restored to previous add book contetn

--- app/src/main/java/com/MCIT/raf/GetBookActivity.java
+++ app/src/main/java/com/MCIT/raf/GetBookActivity.java
@@ -7,12 +7,9 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -25,12 +22,10 @@ import java.util.Calendar;
 
 public class GetBookActivity extends AppCompatActivity {
 
-    static TextView startDateText  ,endDateText , yourPointsTextView;
+    static TextView startDateText  , yourPointsTextView;
     static int  startYear , startMonth , startDay ,
-                endYear , endMonth , endDay,
                 currentYear , currentMonth , currentDay;
     static boolean startDateFlag = false;
-    static boolean endDateFlag = false;
 
     static Button startDateButton;
     @Override
@@ -55,11 +50,6 @@ public class GetBookActivity extends AppCompatActivity {
 
         ScrollView content = (ScrollView) findViewById(R.id.getBook_content);
 
-        TextView bookNameTextView = (TextView)content.findViewById(R.id.BookNameTXT);
-        final Button endDateButton = (Button)content.findViewById(R.id.end_date_button);
-        final RadioButton purchaseBookRadioButton = (RadioButton)content.findViewById(R.id.purchaseBook);
-        final RadioButton borrowBookRadioButton = (RadioButton)content.findViewById(R.id.borrowBook);
-
         ImageView bookImage = (ImageView)content.findViewById(R.id.cover);
         startDateButton = (Button)content.findViewById(R.id.start_date_button);
         final TextView pointsTextView= (TextView)content.findViewById(R.id.points_text);
@@ -67,7 +57,6 @@ public class GetBookActivity extends AppCompatActivity {
         Button getBookButton = (Button)content.findViewById(R.id.getBook_button);
         Button getPointButton = (Button) content.findViewById(R.id.getPoint_button);
         startDateText = (TextView)content.findViewById(R.id.start_date_text);
-        endDateText = (TextView)content.findViewById(R.id.end_date_text);
 
         bookImage.setImageBitmap(BitmapFactory.decodeByteArray(bookCover,0,bookCover.length));
         pointsTextView.setText(Integer.toString(bookPrice));
@@ -81,75 +70,33 @@ public class GetBookActivity extends AppCompatActivity {
             }
         });
 
-        endDateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EndDatePickerFragment dialogFragment = new EndDatePickerFragment();
-                dialogFragment.show(getFragmentManager() , "endDateFlag");
-            }
-        });
 
-        purchaseBookRadioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                startDateButton.setEnabled(false);
-                startDateButton.setText("Delivery date");
-                endDateButton.setEnabled(false);
-//                startDateText.setText("");
-                endDateText.setText("");
-                pointsTextView.setText(Integer.toString(bookPrice));
-                startDateFlag = false;
-                endDateFlag = false;
-            }
-        });
 
-        borrowBookRadioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                startDateButton.setEnabled(true);
-                startDateButton.setText("Start date");
-                endDateButton.setEnabled(true);
-                pointsTextView.setText("");
-            }
-        });
 
 
         getBookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (borrowBookRadioButton.isChecked()) {
-                    if (startDateFlag && endDateFlag) {
-                        if (CurrentUser.getPoints() > bookPrice) {
-                            Request.addBorrowRequest(startYear, startMonth, startDay, endYear,
-                                    endMonth, endDay, bookID, bookPrice, getApplicationContext());
-                            Snackbar.make(v, "processing your request", Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();
-                            yourPointsTextView.setText(Integer.toString(CurrentUser.getPoints()));
-                        } else
-                            Toast.makeText(getApplicationContext(), "you don't have enough coins",
-                                    Toast.LENGTH_LONG).show();
-                    } else
-                        Toast.makeText(getApplicationContext(), "please select valid start and end date",
-                                Toast.LENGTH_LONG).show();
-                } else {
+
                     if (startDateFlag) {
                         if (CurrentUser.getPoints() > bookPrice) {
                             Request.addGetRequest(startYear, startMonth, startDay, bookID,
-                                    bookPrice, getApplicationContext());
+                                                    bookPrice , getApplicationContext());
                             Snackbar.make(v, "processing your request", Snackbar.LENGTH_LONG)
                                     .setAction("Action", null).show();
                             yourPointsTextView.setText(Integer.toString(CurrentUser.getPoints()));
 
-                        } else
+                        }
+                        else
                             Toast.makeText(getApplicationContext(), "you don't have enough coins",
-                                    Toast.LENGTH_LONG).show();
+                                                            Toast.LENGTH_LONG).show();
                     } else {
                         Toast.makeText(getApplicationContext(), "please select valid delivery date",
-                                Toast.LENGTH_LONG).show();
+                                                            Toast.LENGTH_LONG).show();
                     }
                 }
             }
-        });
+        );
 
         getPointButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,30 +105,6 @@ public class GetBookActivity extends AppCompatActivity {
                 GetBookActivity.this.startActivity(getPointIntent);
             }
         });
-
-        TextWatcher textWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int i;
-                if ((i = calculatePoints(bookPrice , purchaseBookRadioButton))!=-1){
-                    pointsTextView.setText(Integer.toString(i));
-                } else
-                    pointsTextView.setText("");
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        };
-
-        startDateText.addTextChangedListener(textWatcher);
-        endDateText.addTextChangedListener(textWatcher);
 
     }
 
@@ -206,15 +129,6 @@ public class GetBookActivity extends AppCompatActivity {
         startDateText.setLayoutParams(lp);
     }
 
-    public static void showEndDate(int year , int month , int day){
-        endYear = year;
-        endMonth = month;
-        endDay = day;
-        endDateFlag = checkEndDate();
-        endDateText.setText(new StringBuilder().append(day).append("/").append(month +1)
-                .append("/").append(year));
-    }
-
 
     private static boolean checkStartDate(){
         if (startYear > currentYear) {
@@ -237,36 +151,6 @@ public class GetBookActivity extends AppCompatActivity {
         return false;
     }
 
-    private static boolean checkEndDate(){
-        if (endYear > currentYear) {
-            endDateText.setTextColor(Color.BLACK);
-            return true;
-        }
-        if (endYear == currentYear){
-            if (endMonth > currentMonth) {
-                endDateText.setTextColor(Color.BLACK);
-                return true;
-            }
-            if (endMonth == currentMonth){
-                if (endDay > currentDay) {
-                    endDateText.setTextColor(Color.BLACK);
-                    return true;
-                }
-            }
-        }
-        endDateText.setTextColor(Color.RED);
-        return false;
-    }
-
-    private int calculatePoints (int bookPrice , RadioButton radioButton){
-        if (radioButton.isChecked())
-            return bookPrice;
-        if (endDateFlag && startDateFlag){
-            //TODO complete this function
-            return (int)bookPrice/2;
-        }
-        return -1;
-    }
 
 
 
