e9c44443b5347cd3a31f2cf58f25900417626fcb
agamal94 <a.gamal94@hotmail.com>
2016-07-13 22:52:32 +0200

    UI improvements on 4 inch devices 2

--- app/src/main/java/com/MCIT/raf/RequestBook.java
+++ app/src/main/java/com/MCIT/raf/RequestBook.java
@@ -2,13 +2,11 @@ package com.MCIT.raf;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 
 import com.MCIT.raf.data.NewBookRequest;
 import com.parse.FindCallback;
@@ -53,7 +51,7 @@ public class RequestBook extends AppCompatActivity {
                         books.add(request.getBookName());
                     }
                 }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),
                         android.R.layout.simple_dropdown_item_1line,books);
                 AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.req_sugg);
                 textView.setAdapter(adapter);
